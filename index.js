074170fa5c512b4b6169bd42f41bc1284208b7c9
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-08-17 12:37:53 -0400

    :space_invader: hello world web

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+
+  const Images = require('src/images');
+
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
