3ae6818b01d707986f978d0bd3731c0eb0a818a0
Nick Doty <npdoty@ischool.berkeley.edu>
2016-08-23 20:36:23 -0700

    fix lint errors

--- script/slack_bot.js
+++ script/slack_bot.js
@@ -4,69 +4,68 @@ Based on the sample bot from http://howdy.ai/botkit
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-require('dotenv').config()
+require('dotenv').config();
 var Botkit = require('botkit');
-var os = require('os');
 
 const Images = require('../src/images');
 
 if (!process.env.SLACKBOT_TOKEN) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
+  console.log('Error: Specify token in environment');
+  process.exit(1);
 }
 
 var controller = Botkit.slackbot({
-    debug: true
+  debug: true
 });
 
-var bot = controller.spawn({
-    token: process.env.SLACKBOT_TOKEN
+controller.spawn({
+  token: process.env.SLACKBOT_TOKEN
 }).startRTM();
 
 
 // use existing hello/shutdown commands from Botkit
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
+controller.hears([ 'hello', 'hi' ], 'direct_message,direct_mention,mention', function(bot, message) {
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  }, function(err) {
+    if (err) {
+      bot.botkit.log('Failed to add emoji reaction :(', err);
+    }
+  });
 });
 
-controller.hears(['shutdown', 'goodbye'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears([ 'shutdown', 'goodbye' ], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    bot.startConversation(message, function(err, convo) {
+  bot.startConversation(message, function(err, convo) {
 
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
+    convo.ask('Are you sure you want me to shutdown?', [
+      {
+        pattern: bot.utterances.yes,
+        callback: function(response, convo) {
+          convo.say('Bye!');
+          convo.next();
+          setTimeout(function() {
+            process.exit();
+          }, 3000);
         }
-        ]);
-    });
+      },
+      {
+        pattern: bot.utterances.no,
+        default: true,
+        callback: function(response, convo) {
+          convo.say('*Phew!*');
+          convo.next();
+        }
+      }
+    ]);
+  });
 });
 
 // listen for any shortcode in the Images list
 controller.hears(new Images().shortcodeRegex(), 'direct_message,direct_mention,mention', function(bot, message) {
-  var e_name = message.match[0].split(":")[1];  
+  var e_name = message.match[0].split(':')[1];  
   var image = new Images().getFromName(e_name);
   if (!image) { return; }
   bot.reply(message, image.toString());
